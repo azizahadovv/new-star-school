8d7cd5c8394553fb2fa5532adb8b8d91036e7155
azizahadovv <123579720+azizahadovv@users.noreply.github.com>
2024-06-21 17:47:46 +0500

    add components in student platforma

--- Front-end/student/tailwind.config.js
+++ Front-end/student/tailwind.config.js
@@ -1,10 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
+  darkMode:"class",
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+
+    extend: {
+     
+      colors: {
+        green: "#41C981",
+        textBlack: "#1D2049",
+        textWhite: "#fff",
+        gray:'#F2F2F2'
+      },
+
+      screens: {
+        minMobil: "310px",
+        // => @media (min-width: 320px) { ... }
+
+        mobil: "425px",
+        // => @media (min-width: 425px) { ... }
+
+        tablet: "700px",
+        tabletIst: "1000px",
+        // => @media (min-width: 640px) { ... }
+
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1280px",
+        // => @media (min-width: 1280px) { ... }
+        fullHd: "1440px",
+      },
+
+    },
+    plugins: [],
+  }
 }
