4ad11693838d16f0677e94d623264f8be0707beb
azizahadovv <aziz.ahadovv@yandex.ru>
2024-08-15 17:14:47 +0500

    mark class

--- Front-end/teacher/src/constanta/style.js
+++ Front-end/teacher/src/constanta/style.js
@@ -1,6 +1,6 @@
 export const Container = 'w-full min-h-[85dvh] tablet:px-10 minMobil:px-1'
 export const styleTopBarUI = 'bg-white border gap-3 border-brGray my-3 rounded-lg flex items-center minMobil:justify-center tablet:justify-start flex-wrap shadow-section-top-class'
 export const styleTopBarUINoFlex = "bg-white border gap-3 border-brGray my-3 rounded-lg shadow-section-top-class"
-export const bgLightGray = 'bg-lightGray  text-[14px] w-[100px] px-3 py-3 rounded-lg border-brGrayborder-btn border-t-2'
+export const bgLightGray = 'bg-lightGray  text-[14px] w-[150px] py-[10px] rounded-lg border-brGrayborder-btn border-t-2'
 export const bgLightGrayMark = 'bg-lightGray py-[10px] text-[14px] w-[100px] px-3 rounded-lg border-brGrayborder-btn'
 export const itemsCenter = 'd-flex align-items-center'
--- Front-end/teacher/src/ui/class schedule.jsx
+++ Front-end/teacher/src/ui/class schedule.jsx
@@ -6,21 +6,37 @@ import { useEffect, useState } from "react"
 function ClassSchedule() {
     const { id } = useParams()
     const [studentsData, setStudentsData] = useState([])
-    const data = `${new Date().getFullYear()}-${("0" + (new Date().getMonth() + 1)).slice(-2)}-${("0" + new Date().getDate()).slice(-2)}`
+    const [studentsMark, setstudentsMark] = useState([])
+    const [studentsMarkIs, setstudentsMarkIs] = useState('')
+
+    const date = `${new Date().getFullYear()}-${("0" + (new Date().getMonth() + 1)).slice(-2)}-${("0" + new Date().getDate()).slice(-2) + " " + new Date().getHours() + ":" + new Date().getMinutes()}`
+    const teacherId = 1;
 
     useEffect(() => {
         students()
     }, [])
-
+    console.log(date);
     const students = async () => {
         const datas = await classInId.myClasses(id)
         setStudentsData(datas.students);
     }
-
-
-
-
-
+    console.log(studentsMark);
+    function studentChangeValue(studentId, gradeValue) {
+        setstudentsMarkIs(gradeValue)
+        if (!studentsMark[studentId]) {
+            studentsMark[studentId] = {
+                "studentId": studentId,
+                "teacherId": teacherId,
+                "subjectId": 0,
+                "termId": 1,
+                "schoolClassId": id,
+                "gradeValue": gradeValue,
+                "dateAssigned": date
+            };
+        } else {
+            studentsMark[studentId].gradeValue = gradeValue;
+        }
+    }
     return (
         <div className="w-full overflow-scroll">
             <table className="table table-hover">
@@ -33,9 +49,9 @@ function ClassSchedule() {
                             O‘quvchilar
                         </th>
                         <th>
-                            <div className={`${bgLightGray} border-t-blue no-underline outline-none`} >{data}</div>
+                            <div className={`${bgLightGray} border-t-blue no-underline outline-none flex items-center justify-center`} >{date}</div>
                         </th>
-                      
+
                     </tr>
                 </thead>
                 <tbody>
@@ -50,13 +66,20 @@ function ClassSchedule() {
                                     </div>
                                 </td>
                                 <td>
-                                    <input value={5} className={`${bgLightGrayMark} no-underline outline-none`} />
+                                    <select style={{ width: "150px" }} value={studentsMark[item.id]?.gradeValue ?? ''} onChange={(e) => studentChangeValue(item.id, e.target.value)} id={item.id} className="form-select">
+                                        <option value={''} hidden>Baholash</option>
+                                        <option value={'present'}>Darsda bor</option>
+                                        <option value={'sababli'}>Sababli</option>
+                                        <option value={'sababsiz'}>Sababsiz</option>
+                                        <option value={'5'}>5</option>
+                                        <option value={'4'}>4</option>
+                                        <option value={'3'}>3</option>
+                                        <option value={'2'}>2</option>
+                                    </select>
                                 </td>
-                             
                             </tr>
                         })
                     }
-
                 </tbody>
             </table>
         </div>
@@ -64,3 +87,7 @@ function ClassSchedule() {
 }
 
 export default ClassSchedule
+
+
+
+{/* <input value={studentsMark[item.id]?.gradeValue ?? ''} onChange={(e) => studentChangeValue(item.id, e.target.value)} className={`${bgLightGrayMark} no-underline outline-none`} /> */ }
