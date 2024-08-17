ea17567b3653e036834501ef414c27f122908350
azizahadovv <123579720+azizahadovv@users.noreply.github.com>
2024-08-17 22:04:56 +0500

    add mark end view maarks student

--- Front-end/teacher/src/components/mark-student.jsx
+++ Front-end/teacher/src/components/mark-student.jsx
@@ -5,9 +5,7 @@ import MarkClass from '../service/mark'
 import { SELECTTERMS } from '../ui'
 function MarkStudents() {
   const [selectedOption, setSelectedOption] = useState('')
-
   const [datas, setDatas] = useState([])
-  const [grades, setGrades] = useState([])
   const { id } = useParams()
 
   useEffect(() => {
@@ -16,8 +14,22 @@ function MarkStudents() {
   const getMarkData = async () => {
     const datas = await MarkClass.getMarks(1, 1, id, selectedOption)
     setDatas(datas)
-console.log(datas);
   }
+
+
+  const dates = datas.map(item => item.date);
+
+  // Create a list of unique students
+  const students = Array.from(
+    new Set(datas.flatMap(item => item.grades.map(grade => grade.studentName)))
+  );
+
+  // Create a helper function to get the grade for a specific student and date
+  const getGrade = (studentName, date) => {
+    const dateData = datas?.find(d => d.date === date);
+    const studentGrade = dateData?.grades.find(g => g.studentName === studentName);
+    return studentGrade ? studentGrade.gradeValue : '';
+  };
 
   return (
     <div className={`${Container}`}>
@@ -25,35 +37,30 @@ console.log(datas);
         <SELECTTERMS selectedOption={selectedOption} setSelectedOption={setSelectedOption} />
       </div>
       <div className={`${styleTopBarUINoFlex} min-h-96 p-3`}>
-        <table className='table table-hover '>
-          <thead>
-            {
-              <tr>
-                <th>Id</th>
-                <th>Name</th>
-                {
-                  datas?.map((item, idx) => {
-                    return <th key={idx}>{item.date}</th>
-                  })
-                }
-              </tr>
-            }
+        {datas.length !== 0 ? <table className="teble">
+          <thead className='table-hover'>
+            <tr>
+              <th className="py-2 px-4 border-b">№</th>
+              <th className="py-2 px-4 border-b">O'quvchilar</th>
+              {dates?.map((date, index) => (
+                <th key={index} className="py-2 px-4 border-b">{date}</th>
+              ))}
+            </tr>
           </thead>
           <tbody>
-            {
-              datas?.map((item) => {
-                return item.grades.map((grade, idx) => {
-                  return <tr key={idx}>
-                    <td>{idx + 1}</td>
-                    <td>{grade.studentName}</td>
-                    <td>{grade.gradeValue}</td>
-                  </tr>
-                })
-              })
-
-            }
+            {students?.map((student, index) => (
+              <tr key={index}>
+                <td className="py-2 px-4 border-b">{index+1}</td>
+                <td className="py-2 px-4 border-b">{student}</td>
+                {dates?.map((date, i) => (
+                  <td key={i} className="py-2 px-4 border-b text-center">
+                    {getGrade(student, date)}
+                  </td>
+                ))}
+              </tr>
+            ))}
           </tbody>
-        </table>
+        </table> : <div className='w-full h-full flex items-center justify-center mt-5 text-3xl'>Ma'lumot topilmadi</div>}
       </div>
     </div>
   )
--- Front-end/teacher/src/template/sidebar.jsx
+++ Front-end/teacher/src/template/sidebar.jsx
@@ -1,18 +1,17 @@
 import { useDispatch } from 'react-redux';
 import { BUTTONEXIT, BUTTONSIDEBAR, LANGUAGEPOTION } from '../ui';
-import { Link, useNavigate } from 'react-router-dom';
-import { infoUser, infoUserOutline, LISTCLASS, LISTCLASSOUTLINE, logo, savatcha, savatchaOutline } from '../icons';
+import { Link } from 'react-router-dom';
+import { infoUser, infoUserOutline, LISTCLASS, LISTCLASSOUTLINE, logo } from '../icons';
 import { openVisible } from '../slice/sidebar';
 function Sidebar() {
   const dispatch = useDispatch()
-  const navitagate = useNavigate()
   return (
     <div className='p-3 d-flex flex-col gap-3 w-full h-screen bg-darkGray'>
       <div className="offcanvas-header border-b border-white d-flex align-items-center justify-content-between py-3">
         <Link to={"/"} onClick={() => dispatch(openVisible())} className="offcanvas-title" id="offcanvasDarkNavbarLabel"><img className='w-36' src={logo} alt="logo" /></Link>
         <button onClick={() => dispatch(openVisible())} className="btn-close btn-close-white"></button>
       </div>
-      <div className="flex justify-center flex-col items-stretch">
+      <div className="flex justify-center flex-col items-stretch ">
         <div className="flex flex-col justify-center items-stretch gap-2">
           <BUTTONSIDEBAR barVisible={() => dispatch(openVisible())} slug={'/list-of-classes'} img={LISTCLASS} img2={LISTCLASSOUTLINE} name={"Sinflar ro'yhati"} />
           <BUTTONSIDEBAR barVisible={() => dispatch(openVisible())} slug={'/profile'} img={infoUser} img2={infoUserOutline} name={"Shaxsiy malumotlar"} />
@@ -21,7 +20,7 @@ function Sidebar() {
           <LANGUAGEPOTION />
         </div>
         <div className='mt-4'>
-        <BUTTONEXIT />
+          <BUTTONEXIT />
         </div>
       </div>
     </div>
